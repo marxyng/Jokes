4fe8fb1403cfaa44962ec98dd946e1f058c6a300
unknown <marxyng01@hotmail.com>
2022-10-08 00:37:44 -0500

    first commit

---
+++ JokesWebApp2/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JokesWebApp2.Models;
+
+namespace JokesWebApp2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebApp2.Models.Joke> Joke { get; set; }
+    }
+}
